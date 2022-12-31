acbfd2380905ec65d0c37389f0dbeaf00bcff275
gs999918 <64943615+gs999918@users.noreply.github.com>
2022-12-30 16:33:08 -1000

    Update model.py

--- src/model.py
+++ src/model.py
@@ -158,8 +158,8 @@ def model(hparams, X, past=None, scope='model', reuse=False):
 
         # Transformer
         presents = []
-         pasts = tf.unstack(past, axis=1) if past is not None #else hparams['n_layer']
-         assert len(pasts) < hparams['n_layer']
+        pasts = tf.unstack(past, axis=1) if past is not None #else hparams['n_layer']
+        assert len(pasts) < hparams['n_layer']
          for layer, past in enumerate(pasts):
             h, present = block(h, 'h%d' % layer, past=past, hparams=hparams)
             presents.append(present)
